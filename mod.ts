c252641ccdbd405a751907476c3e2ee45195175b
Kevin Gawlik <kevin@gawlik.io>
2022-03-29 18:58:22 +0200

    add: basic deno server

---
+++ mod.ts
@@ -0,0 +1,9 @@
+import { serve } from "https://deno.land/std/http/server.ts";
+
+const handler = (_req: Request): Response => {
+  return new Response("Hello world");
+};
+
+console.log("Listening on http://localhost:8000");
+
+serve(handler);
